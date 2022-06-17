55642e100178aa3f540dd9e2611b8e96e9db0766
Sanjana Jadhav <sxj190054@utdallas.edu>
2022-06-17 14:21:19 -0500

    add firestore

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+var firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBI5ycy5yNanO_xw4c5bX2apgZmxw-rztM",
+  authDomain: "wmwe-website.firebaseapp.com",
+  projectId: "wmwe-website",
+  storageBucket: "wmwe-website.appspot.com",
+  messagingSenderId: "886469531612",
+  appId: "1:886469531612:web:709620fe1f7a5bf334e119",
+});
+
+var db = firebaseApp.firestore();
+
+export { db };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
+import "../App.css";
+import { db } from "../Firebase";
 
-function Contact(props) {
-    return (
-        <div>
-             <h1>This is the Contact Us Page</h1>
-        </div>
-    );
-}
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  return (
+    <div>
+      <h1>This is the About Page</h1>
+      <form className="form">
+        <p>we'd love to hear from you</p>
+
+        <label>name</label>
+        <input placeholder="name" value={name} onChange={(e)}/>
+
+        <label>email</label>
+        <input placeholder="email" value={email}/>
+
+        <label>message</label>
+        <textarea placeholder="message"></textarea>
+
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
