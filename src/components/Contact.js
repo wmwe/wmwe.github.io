475e655d5e5bc3306e8a93f3fd53f47b1a2f94c1
Sanjana Jadhav <sxj190054@utdallas.edu>
2022-06-17 14:46:17 -0500

    functional firestore database

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,20 +7,53 @@ const Contact = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    db.collection("contact-form").add({
+        name: name,
+        email: email,
+        message: message,
+    })
+    .then(() => {
+        alert("Submitted!");
+    })
+    .catch((error) => {
+        alert(error.message);
+    })
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
     <div>
       <h1>This is the About Page</h1>
-      <form className="form">
+
+      <form className="form" onSubmit={handleSubmit}>
         <p>we'd love to hear from you</p>
 
         <label>name</label>
-        <input placeholder="name" value={name} onChange={(e)}/>
+        <input
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
         <label>email</label>
-        <input placeholder="email" value={email}/>
+        <input
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label>message</label>
-        <textarea placeholder="message"></textarea>
+        <textarea
+          placeholder="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
 
         <button type="submit">submit</button>
       </form>
