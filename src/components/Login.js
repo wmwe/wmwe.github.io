09b8bd5ec4ce21804bcea1815f7f5b2ea346ae99
Manasi Vipat <manivipat9@gmail.com>
2022-06-17 18:18:11 -0500

    basic login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,22 @@
-import React from 'react';
-
-function Login(props) {
-    return (
-        <div>
-             <h1>This is the Login Page</h1>
-        </div>
-    );
+import React, {useState} from 'react';
+import './style.css'
+function Login() {
+    return(
+      <div className="form">
+          <div className="form-body">
+              <div className="email">
+                  <label className="form__label" for="email">email </label>
+                  <input  type="email" id="email" className="form__input" placeholder="email"/>
+              </div>
+              <div className="password">
+                  <label className="form__label" for="password">password </label>
+                  <input className="form__input" type="password"  id="password" placeholder="password"/>
+              </div>
+          </div>
+          <div class="footer">
+              <button type="submit" class="btn">log in</button>
+          </div>
+      </div>      
+    )       
 }
-
 export default Login;
