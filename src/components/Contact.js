2a89b29681f2595cad0ec0e100e528283ecd2fea
Sanjana Jadhav <sxj190054@utdallas.edu>
2022-06-26 23:59:23 -0500

    fix UI + add image

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import "../App.css";
+import "./ContactForm.css";
 import { db } from "../Firebase";
+import women_pic from "./women.png";
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -36,38 +37,42 @@ const Contact = () => {
 
   return (
     <div>
-      <div className="container">
-        <h2 className="header">contact us.</h2>
-        <p className="text">we'd love to hear from you</p>
-        <form className="form" onSubmit={handleSubmit}>
-          <label>name</label>
-          <input
-            placeholder="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+      <div className="main" >
+        <img className="photo" src={women_pic} />
 
-          <label>email</label>
-          <input
-            placeholder="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+        <div className="container">
+          <h2 className="header">contact us.</h2>
+          <p className="text">we'd love to hear from you</p>
+          <form className="form" onSubmit={handleSubmit}>
+            <label>name</label>
+            <input
+              placeholder="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
 
-          <label>message</label>
-          <textarea
-            placeholder="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          ></textarea>
+            <label>email</label>
+            <input
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-          <button
-            type="submit"
-            style={{ background: loader ? "#ccc" : "rgb(0, 0, 196)" }}
-          >
-            {buttonText}
-          </button>
-        </form>
+            <label>message</label>
+            <textarea
+              placeholder="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+
+            <button
+              type="submit"
+              style={{ background: loader ? "#6DCFF6" : "rgb(0, 0, 196)" }}
+            >
+              {buttonText}
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
