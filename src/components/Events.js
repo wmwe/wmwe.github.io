15d97ea0d54023dc1e5eb0cb326db6de60c7f100
Sanjana Jadhav <sxj190054@utdallas.edu>
2022-07-02 01:40:37 -0500

    add event info

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import "./EventsStyle.css";
 
 function Events(props) {
-    return (
-        <div>
-             <h1>This is the Events Page</h1>
+  return (
+    <div>
+        <p className="header">upcoming events.</p>
+        <div className="event_block_1">
+            <p className="event_font">midsemester&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            15</p>
+            <p className="event_info">7:00 pm
+            <br></br>
+            ECSW 1.315</p>
         </div>
-    );
+
+        <div className="event_block_2">
+            <p className="event_font">JS workshop&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            20</p>
+            <p className="event_info">7:00 pm
+            <br></br>
+            ECSW 1.315</p>
+        </div>
+
+        <div className="event_block_3">
+            <p className="event_font">end of semester&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            29</p>
+            <p className="event_info">7:00 pm
+            <br></br>
+            ECSW 1.315</p>
+        </div>
+    </div>
+  );
 }
 
 export default Events;
