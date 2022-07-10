13fd99c170214c3c3ae8467289384d54e76ae962
Sanjana Jadhav <sxj190054@utdallas.edu>
2022-07-10 01:00:03 -0500

    update calendar 2

--- src/components/Events.js
+++ src/components/Events.js
@@ -51,7 +51,7 @@ function Events(props) {
       <iframe
         className="calendar"
         src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%236dcff6&amp;ctz=America%2FChicago&amp;showTitle=0&amp;showNav=1&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=1&amp;src=M3VxaThqNTE1dXVjNXZrY3EwcTBvdml1bW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%231b3a5d"
-        style="border-width:0"
+        style="border:solid 1px #1b3a5d"
         style={{
           width: "46%",
           height: "60vh",
