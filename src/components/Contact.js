5ec048128107a49f46e4d1a032b79c234d47e205
Sanjana Jadhav <sxj190054@utdallas.edu>
2022-10-06 00:11:59 -0500

    add flex but still need to add icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -39,71 +39,57 @@ const Contact = () => {
 
   return (
     <div>
-      <div className="main">
-        <div className="container_left">
+      <div className="outer-container">
+        <div className="containerImage">
           <img className="photo" src={women_pic} />
+          
+          <br></br>
+          <br></br>
           <br></br>
 
-          <div className="new_icons">
-            <img className="icons" src={email_pic} />
-            <br></br>
-            &nbsp;
-            <img className="icons_2" src={location_pic} />
-          </div>
 
-          <div className="new_links">
-            
-            <a
-              className="link"
-              target="blank"
-              href="mailto: wmweatutd@gmail.com"
-            >
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;wmweatutd@gmail.com
-            </a>
-            <br></br>
-            <br></br>
-            <a
-              className="link"
-              target="blank"
-              href="https://www.google.com/maps/place/University+of+Texas+at+Dallas/@32.9857019,-96.7524361,17z/data=!3m1!4b1!4m5!3m4!1s0x864c21ff895e4aa5:0xd9098b32e9aa1331!8m2!3d32.9856974!4d-96.7502474"
-            >
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;800 W Campbell Rd
-            </a>
-          </div>
+
         </div>
 
-        <div className="container">
-          <h2 className="header">contact us.</h2>
-          <p className="text">we'd love to hear from you!</p>
-          <form className="form" onSubmit={handleSubmit}>
-            <label>name</label>
-            <input
-              placeholder="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+        <div className="containerForm">
+         <h2 className="header">contact us.</h2>
+         <p className="text">we'd love to hear from you!</p>
+         <br></br>
+         <br></br>
+         <br></br>
+         <form className="form" onSubmit={handleSubmit}>
+           <label>name</label>
+           <input
+             placeholder="name"
+             value={name}
+             onChange={(e) => setName(e.target.value)}
+           />
+ 
+           <label>email</label>
+           <input
+             placeholder="email"
+             value={email}
+             onChange={(e) => setEmail(e.target.value)}
+           />
+ 
+           <label>message</label>
+           <textarea
+             placeholder="message"
+             value={message}
+             onChange={(e) => setMessage(e.target.value)}
+           ></textarea>
+ 
+           <button
+             type="submit"
+             style={{ background: loader ? "#6DCFF6" : "#1B3A5D" }}
+           >
+             {buttonText}
+           </button>
+         </form>
 
-            <label>email</label>
-            <input
-              placeholder="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+         <br></br>
+         <br></br>
 
-            <label>message</label>
-            <textarea
-              placeholder="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            ></textarea>
-
-            <button
-              type="submit"
-              style={{ background: loader ? "#6DCFF6" : "#1B3A5D" }}
-            >
-              {buttonText}
-            </button>
-          </form>
         </div>
       </div>
     </div>
