002a851c77ad11d248ee60487053aeae4c906f57
Sanjana Jadhav <sxj190054@utdallas.edu>
2023-03-31 14:03:53 -0500

    contact us: using flex (still need to make responsive)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -41,45 +41,45 @@ const Contact = () => {
   return (
     <div>
       <Banner></Banner>
-      <div className="main">
-        <div className="container_left">
-          <img className="photo" src={women_pic} />
-          <br></br>
-
-          <div className="new_icons">
-            <img className="icons" src={email_pic} />
-            <br></br>
-            &nbsp;
-            <img className="icons_2" src={location_pic} />
+      <div className="outer-container-contact">
+        <div className="containerImage-contact">
+          <div className="img-div-contact">
+            <img className="photo-contact" src={women_pic} />
           </div>
+          <div className="icons-div-contact">
+            <div className="icons-div-contact-left">
+              <img className="icons-contact" src={email_pic} />
+              <br></br>
+              <img className="icons-contact" src={location_pic} />
+            </div>
 
-          <div className="new_links">
-            <a
-              className="link"
-              target="blank"
-              href="mailto: wmweatutd@gmail.com"
-            >
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;wmweatutd@gmail.com
-            </a>
-            <br></br>
-            <br></br>
-            <a
-              className="link"
-              target="blank"
-              href="https://www.google.com/maps/place/University+of+Texas+at+Dallas/@32.9857019,-96.7524361,17z/data=!3m1!4b1!4m5!3m4!1s0x864c21ff895e4aa5:0xd9098b32e9aa1331!8m2!3d32.9856974!4d-96.7502474"
-            >
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;800
-              W Campbell Rd
-            </a>
+            <div className="icons-div-contact-right">
+              <a
+                className="link"
+                target="blank"
+                href="mailto: wmweatutd@gmail.com"
+              >
+                wmweatutd@gmail.com
+              </a>
+              <br></br>
+              <br></br>
+              <a
+                className="link"
+                target="blank"
+                href="https://www.google.com/maps/place/University+of+Texas+at+Dallas/@32.9857019,-96.7524361,17z/data=!3m1!4b1!4m5!3m4!1s0x864c21ff895e4aa5:0xd9098b32e9aa1331!8m2!3d32.9856974!4d-96.7502474"
+              >
+                800 W Campbell Rd
+              </a>
+            </div>
           </div>
         </div>
 
-        <div className="container1">
-          <h2 className="header">contact us.</h2>
+        <div className="containerForm-contact">
           <br></br>
           <br></br>
-          <p className="text">we'd love to hear from you!</p>
-          <form className="form" onSubmit={handleSubmit}>
+          <h2 className="header-contact">contact us.</h2>
+          <p className="text-contact">we'd love to hear from you!</p>
+          <form className="form-contact" onSubmit={handleSubmit}>
             <label>name</label>
             <input
               placeholder="name"
@@ -108,6 +108,8 @@ const Contact = () => {
               {buttonText}
             </button>
           </form>
+          <br></br>
+          <br></br>
         </div>
       </div>
     </div>
