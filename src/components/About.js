cdd3742706d836704d6f432f02d2cb0fbeebfbe4
vainall <vaishunalla@yahoo.com>
2023-04-05 18:33:28 -0500

    fixed fonts on about page

--- src/components/About.js
+++ src/components/About.js
@@ -13,12 +13,12 @@ function About(props) {
         <div className= "textbody">
             <h1 className='aboutheader'>about us.</h1>
              <div className= "mission">
-             <h2 className='header'>our mission.</h2>
-             <p>WMWE aims to improve the retention rates of women 
+             <h2 className='section__header'>our mission.</h2>
+             <h3 className='body2' style={{width: "100%"}}>WMWE aims to improve the retention rates of women 
                 in tech and STEM fields by giving them a head start 
                 on making valuable connections and gaining advice from 
                 seasoned professionals who know their way around the industry. 
-                We are firm believers in empowered women empowering women!</p>
+                We are firm believers in empowered women empowering women!</h3>
              </div>
             
                 <div className="missionpics">
@@ -29,23 +29,23 @@ function About(props) {
                 </div>
 
              <div className="pillars">
-             <h2 className='header'>our pillars.</h2>
-             <p><strong>Support.</strong> We strive to provide support for all women through each other. 
+             <h2 className='aboutheader' style={{marginLeft:"0%"}}>our pillars.</h2>
+             <h3 className='body2' style={{width: "100%"}}><strong>Support.</strong> <br></br>We strive to provide support for all women through each other. 
                 Your mentor, team, and everyone in WMWE is there to help you out 
                 whenever you need it. Before we are a University Wide Organization, 
-                we are just a group of people that want to help!</p>
+                we are just a group of people that want to help!</h3>
 
-             <p><strong>Empowerment.</strong> In the STEM field, women make up only 24% of the entire 
+                <h3 className='body2' style={{width: "100%"}}><strong>Empowerment.</strong> <br></br> In the STEM field, women make up only 24% of the entire 
                 workforce, with the percentage only decreasing with higher positions.  
                 In WMWE, we strive to empower everyone and help make them stronger and more confident 
-                in whatever they do. After all, our slogan is “Empowered Women Empowering Women”.</p>
+                in whatever they do. After all, our slogan is “Empowered Women Empowering Women”.</h3>
 
-             <p><strong>Mentorship.</strong> By linking you up with a professional 
+             <h3 className='body2' style={{width: "100%"}}><strong>Mentorship.</strong> <br></br>By linking you up with a professional 
                 woman who is working in the STEM field in a semester long mentorship, 
                 you will learn everything you wanted to know. From professionalism in the workplace, 
                 to things you can do to upstart your career. In the mentee form, we ask for your major 
                 and what fields of that major you are interested in so we can pair you up with a mentor who 
-                is doing exactly that. At the end of the semester, you will be walking out with tons of more knowledge!</p>
+                is doing exactly that. At the end of the semester, you will be walking out with tons of more knowledge!</h3>
 
              </div>
 
@@ -57,14 +57,14 @@ function About(props) {
                 <img className='mentorship' src={mentorshipwithborder}/>
              </div>
 
-             <div className="history">
-                <h2 className='header'>history.</h2>
-                <p>Founded at the University of Texas at Dallas in the summer of 2016, 
+             <div className="history" style={{paddingBottom: "7%"}}>
+                <h2 className='aboutheader' style={{marginLeft:"0%"}}>history.</h2>
+                <h3 className='body2' style={{width: "100%"}}>Founded at the University of Texas at Dallas in the summer of 2016, 
                     Women Mentoring Women in Engineering is a mentoring program that fosters 
                     mentorships between students of UTD and working women. Students are paired 
                     up with professional women currently working in the technology industry or in 
                     academia to be mentored for a semester and build a relationship with those 
-                    familiar with the working environment.</p>
+                    familiar with the working environment.</h3>
 
              </div>
         </div>
